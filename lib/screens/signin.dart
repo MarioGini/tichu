c70ca2ecffc525049e6718dab8dabefc9a2b6f8b
Mario Gini <mgini@hispeed.ch>
2020-04-08 19:14:41 +0200

    google authentication working on andoid

--- lib/screens/signin.dart
+++ lib/screens/signin.dart
@@ -4,6 +4,10 @@ import 'package:google_sign_in/google_sign_in.dart';
 final FirebaseAuth _auth = FirebaseAuth.instance;
 final GoogleSignIn googleSignIn = GoogleSignIn();
 
+String name;
+String email;
+String imageUrl;
+
 Future<String> signInWithGoogle() async {
   final GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
   final GoogleSignInAuthentication googleSignInAuthentication =
@@ -22,6 +26,19 @@ Future<String> signInWithGoogle() async {
 
   final FirebaseUser currentUser = await _auth.currentUser();
   assert(user.uid == currentUser.uid);
+
+  assert(user.email != null);
+  assert(user.displayName != null);
+  assert(user.photoUrl != null);
+
+  name = user.displayName;
+  email = user.email;
+  imageUrl = user.photoUrl;
+
+// Only taking the first part of the name, i.e., First Name
+  if (name.contains(" ")) {
+    name = name.substring(0, name.indexOf(" "));
+  }
 
   return 'signInWithGoogle succeeded: $user';
 }

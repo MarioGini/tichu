a877fa6c7c245e34f78ff187241dc394d9b9158b
Mario Gini <mgini@hispeed.ch>
2020-04-11 00:08:47 +0200

    facebook auth works on android

--- lib/screens/signin.dart
+++ lib/screens/signin.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:flutter_facebook_login/flutter_facebook_login.dart';
 
 final FirebaseAuth _auth = FirebaseAuth.instance;
 final GoogleSignIn googleSignIn = GoogleSignIn();
@@ -9,14 +10,20 @@ String email;
 String imageUrl;
 
 Future<String> signInWithGoogle() async {
-  final GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
-  final GoogleSignInAuthentication googleSignInAuthentication =
-      await googleSignInAccount.authentication;
+  final facebookLogin = FacebookLogin();
+  final result = await facebookLogin.logIn(['email']);
+  final token = result.accessToken.token;
+  AuthCredential credential =
+      FacebookAuthProvider.getCredential(accessToken: token);
 
-  final AuthCredential credential = GoogleAuthProvider.getCredential(
-    accessToken: googleSignInAuthentication.accessToken,
-    idToken: googleSignInAuthentication.idToken,
-  );
+  // final GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
+  // final GoogleSignInAuthentication googleSignInAuthentication =
+  //     await googleSignInAccount.authentication;
+
+  // final AuthCredential credential = GoogleAuthProvider.getCredential(
+  //   accessToken: googleSignInAuthentication.accessToken,
+  //   idToken: googleSignInAuthentication.idToken,
+  // );
 
   final AuthResult authResult = await _auth.signInWithCredential(credential);
   final FirebaseUser user = authResult.user;

5e9abab7382c13d733b3b56e3e2987dacbe881a4
Mario Gini <mgini@hispeed.ch>
2026-04-04 19:03:20 +0200

    fix

--- test/headless/rl_policy_builder_test.dart
+++ test/headless/rl_policy_builder_test.dart
@@ -44,13 +44,13 @@ void main() {
       ];
       await transitionsFile.writeAsString('${lines.join('\n')}\n');
 
-      final processResult = await Process.run('dart', [
+      final processResult = await Process.run(Platform.resolvedExecutable, [
         'run',
         'tool/rl/build_policy_from_transitions.dart',
         '--input=${transitionsFile.path}',
         '--output=${policyFile.path}',
         '--gamma=0.5',
-      ], workingDirectory: '/home/mario/repos/tichu');
+      ], workingDirectory: Directory.current.path);
 
       expect(
         processResult.exitCode,
